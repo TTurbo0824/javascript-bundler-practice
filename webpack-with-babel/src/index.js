9e88a5af96ad279b14732a098600b6c6e404bcae
Kyungjoo Ha <429why@gmail.com>
2022-03-13 14:28:40 +0900

    add: Function to add cocktail name & image

--- webpack-with-babel/src/index.js
+++ webpack-with-babel/src/index.js
@@ -1,4 +1,7 @@
-const apiURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+import {createElementWithClass as create} from './utilities.js';
+
+const apiURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
 async function getDrinks() {
   const response = await fetch(`${apiURL}martini`);
   const data = await response.json();
@@ -6,4 +9,16 @@ async function getDrinks() {
   return data;
 }
 
-getDrinks().then((drink) => console.log(drink));
+getDrinks().then((drink) => {
+  drink.drinks.forEach((el) => {
+    const root = document.querySelector('#root');
+    const drinkImg = create('img', 'cocktail-image');
+    drinkImg.src = el.strDrinkThumb;
+
+    const title = create('div');
+    title.innerHTML = el.strDrink;
+    root.appendChild(title);
+    root.appendChild(drinkImg);
+    return root;
+  });
+});
